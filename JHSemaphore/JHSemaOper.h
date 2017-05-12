3dacc4007093b49a05af9db7e0d44f110d456f00
xuejh <327646128@qq.com>
2017-05-12 16:44:00 +0800

    提交。。。。。。。。。

---
+++ JHSemaphore/JHSemaOper.h
@@ -0,0 +1,14 @@
+//
+//  JHSemaOper.h
+//  Pods
+//
+//  Created by Xuejinhui on 17/5/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JHSemaOper : NSObject
+
+- (void)log;
+@end
---
+++ JHSemaphore/JHSemaOper.m
@@ -0,0 +1,17 @@
+//
+//  JHSemaOper.m
+//  Pods
+//
+//  Created by Xuejinhui on 17/5/12.
+//
+//
+
+#import "JHSemaOper.h"
+
+@implementation JHSemaOper
+
+- (void)log
+{
+    NSLog(@"aa");
+}
+@end
